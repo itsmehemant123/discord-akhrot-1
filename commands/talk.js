422668d3aa3466ffa8e271a21407ed594b16cd47
Hemanth Devarapalli <itsmehemant123@gmail.com>
2017-11-02 16:58:46 -0400

    Fixed DM talking;

--- commands/talk.js
+++ commands/talk.js
@@ -6,16 +6,19 @@ const chatterServer = require('../config/py-server.json')
 class TalkCommand extends Command {
     constructor() {
         super('talk', {
+          channelRestriction: 'guild',
           trigger: /.*/,
         });
     }
 
     exec(message) {
-      var luck = Math.random() * 100;
-      var trigerred = message.content.toLowerCase().indexOf('wheatley') >= 0;
-      if (trigerred || luck > 35) {
-        logger.info('TRIGERRED BY CHANCE FOR:', message.content)
-        this.triggerBot(message, trigerred);
+      if (message.channel.type != 'dm') {
+        var luck = Math.random() * 100;
+        var trigerred = message.content.toLowerCase().indexOf('wheatley') >= 0;
+        if (trigerred || luck > 35) {
+          logger.info('TRIGERRED FOR:', message.content)
+          this.triggerBot(message, trigerred);
+        }
       }
     }
 
