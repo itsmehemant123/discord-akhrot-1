01a206b27b04a10fc64a3dfa3ed788106c686509
Hemanth Devarapalli <itsmehemant123@gmail.com>
2017-11-03 13:15:17 -0400

    8ball akhrot;

--- commands/talk.js
+++ commands/talk.js
@@ -12,14 +12,27 @@ class TalkCommand extends Command {
     }
 
     exec(message) {
+      logger.info('TRIGE');
       if (message.channel.type != 'dm') {
-        message.channel.startTyping();
         var luck = Math.random() * 100;
-        var trigerred = message.content.toLowerCase().indexOf('wheatley') >= 0;
-        if (trigerred || luck > 35) {
-          logger.info('TRIGERRED FOR:', message.content)
-          this.triggerBot(message, trigerred);
+        if (message.content.indexOf('wheatley, do you think') >= 0) {
+          if (luck > 66) {
+            return message.reply('yes bby');
+          } else if (luck <= 66 && luck > 33) {
+            return message.reply('fuck no my dewd');
+          } else {
+            return message.reply('idk man');
+          }
+        } else {
+
+          var trigerred = message.content.toLowerCase().indexOf('wheatley') >= 0;
+          if (trigerred || luck > 35) {
+            logger.info('TRIGERRED FOR:', message.content)
+            message.channel.startTyping();
+            this.triggerBot(message, trigerred);
+          }
         }
+
       }
     }
 
@@ -33,16 +46,16 @@ class TalkCommand extends Command {
       client.post('http://' + chatterServer.host + ':' + chatterServer.port + '/respond', args, function(data, response) {
         try {
           var payload = JSON.parse(data);
+          message.channel.stopTyping();
           if (trigerred || parseFloat(payload.confidence) > 0.80) {
-            logger.info('SENDING')
-            message.channel.stopTyping();
+            logger.info('SENDING');
             return message.channel.send(payload.message).catch(function (err) {
-              logger.error('ERROR: ', err)
+              logger.error('ERROR: ', err);
             });
           }
         } catch (e) {
           message.channel.stopTyping();
-          logger.log('ERROR PARSING RECIEVED RESPONSE: ', e)
+          logger.log('ERROR PARSING RECIEVED RESPONSE: ', e);
         }
       });
     }
